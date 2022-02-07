2ef50e60c80eceeb63fe47fdcfc3e09497e6cef1
David Chalé Sánchez <personal.contact.dcs@gmail.com>
2022-02-07 17:45:22 -0600

    creation. Building frontend blade

---
+++ resources/js/components/AddToCart.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <hr />
+        <button class="btn btn-warning text-center">Añadir al Carrito</button>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("Component mounted.");
+    },
+};
+</script>
